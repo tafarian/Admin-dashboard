dd5843fa268b232cd0c709dc97d706a5f3244033
Jakub Wyczesany <ghaelios@gmail.com>
2018-04-18 16:08:34 +0200

    creating react components

--- js/app.jsx
+++ js/app.jsx
@@ -4,11 +4,52 @@ import './../css/style.scss';
 
 document.addEventListener('DOMContentLoaded', function(){
 
+    class SideBar extends React.Component {
+        render() {
+            return (
+                <div className="col-sm-3">
+                    <h3>Admin dashboard</h3>
+                    <ul className="list-group">
+                        <li className="list-group-item">Dashboard</li>
+                        <li className="list-group-item">Email</li>
+                        <li className="list-group-item">Calendar</li>
+                        <li className="list-group-item">Charts</li>
+                        <li className="list-group-item">Forms</li>
+                        <li className="list-group-item">Tables</li>
+                        <li className="list-group-item">Maps</li>
+                    </ul>
+                </div>
+            )
+        }
+    }
+
+    class Main extends React.Component {
+        render() {
+            return (
+                <div className="col-sm-9">
+                    <div className="container">
+                        <div className="row">
+                            <h3 className="text">TEST</h3>
+                        </div>
+                        <div className="row bordero">
+                            <div className="container mainbox">
+                                asdasd
+                            </div>
+                        </div>
+                    </div>  
+                </div> 
+            )
+        }
+    }
+
     class App extends React.Component {
         render() {
             return (
-                <div className="main">
-                    test
+                <div className="container">
+                    <div className="row">
+                        <SideBar/>
+                        <Main/>
+                    </div>
                 </div>
                 )
         }
