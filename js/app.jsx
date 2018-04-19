7a339bbdcc3af862cc8f87c412a91802ce732e7a
Jakub Wyczesany <ghaelios@gmail.com>
2018-04-19 15:57:50 +0200

    adding charts

--- js/app.jsx
+++ js/app.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './../css/style.scss';
 
+// import charts
+import {Bar, Line, Pie} from 'react-chartjs-2';
+
 document.addEventListener('DOMContentLoaded', function(){
 
     class SideBar extends React.Component {
@@ -24,6 +27,53 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     class Main extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                usersChartData: {
+                    labels: ['January', 'Ferbuary', 'March', 'April'],
+                    datasets: [
+                        {
+                            label: 'Users online',
+                            data: [
+                                350,
+                                300,
+                                400,
+                                280
+                            ],
+                                backgroundColor: [
+                                    'rgba(255, 99, 132, 0.6)',
+                                    'rgba(255, 99, 132, 0.6)',
+                                    'rgba(255, 99, 132, 0.6)',
+                                    'rgba(255, 99, 132, 0.6)',
+                                ]
+                        }
+                    ]
+                },
+                viewsChartData: {
+                    labels: ['January', 'Ferbuary', 'March', 'April'],
+                    datasets: [
+                        {
+                            label: 'Views',
+                            data: [
+                                1200,
+                                1600,
+                                1250,
+                                2400
+                            ],
+                                backgroundColor: [
+                                    'rgba(22, 87, 25, 0.6)',
+                                    'rgba(22, 87, 25, 0.6)',
+                                    'rgba(22, 87, 25, 0.6)',
+                                    'rgba(22, 87, 25, 0.6)',
+                                ]
+                        }
+                    ]
+                }
+            }
+        }
+        
+        
         render() {
             return (
                 <div className="col-sm-9">
@@ -33,7 +83,16 @@ document.addEventListener('DOMContentLoaded', function(){
                         </div>
                         <div className="row bordero">
                             <div className="container mainbox">
-                                asdasd
+                                <div className="boxTop">
+                                    <Bar
+                                        data={this.state.usersChartData}
+                                        options={{}}/>
+                                </div>
+                                <div className="boxTop">
+                                    <Line
+                                        data={this.state.viewsChartData}
+                                        options={{}}/>
+                                </div>
                             </div>
                         </div>
                     </div>  
