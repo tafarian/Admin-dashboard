9cbc4d3a9afd93fdd81a3df355a48aaa110d5bd1
Jakub Wyczesany <ghaelios@gmail.com>
2018-04-20 14:47:15 +0200

    dependencies and maps added

--- js/Maps.jsx
+++ js/Maps.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+// import maps
+import GoogleMapReact from 'google-map-react'
+
+const AnyReactComponent = ({ text }) => <div>{ text }</div>;
+
 class Maps extends React.Component {
+    static defaultProps = {
+        center: { lat: 50.062044, lng: 19.936721 },
+        zoom: 11
+      }
+   
     render() {
+
         return (
             <div className="col-sm-9">
             <div className="container">
@@ -10,7 +21,14 @@ class Maps extends React.Component {
                 </div>
                 <div className="row bordero">
                     <div className="container mainbox">
-                        
+                    <GoogleMapReact
+                        defaultCenter={ this.props.center }
+                        defaultZoom={ this.props.zoom }>
+                        <AnyReactComponent
+                            lat={ 50.062044 }
+                            lng={ 19.936721 }
+                        />
+                    </GoogleMapReact>
                     </div>
                 </div>
             </div>  
