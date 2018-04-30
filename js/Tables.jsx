58a657895e32185da166c03dcbfbdb95968b8bb2
Jakub Wyczesany <ghaelios@gmail.com>
2018-04-30 15:12:48 +0200

    tables

--- js/Tables.jsx
+++ js/Tables.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+// import tables
+
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+
 class Tables extends React.Component {
     render() {
+
+        let products = [{
+            id: 1,
+            name: "Product1",
+            price: 120
+        }, {
+            id: 2,
+            name: "Product2",
+            price: 80
+        }, {
+            id: 3,
+            name: "Product3",
+            price: 40
+        }, {
+            id: 3,
+            name: "Product3",
+            price: 20
+        }];
+      
+
         return (
             <div className="col-sm-9">
             <div className="container">
@@ -10,7 +34,11 @@ class Tables extends React.Component {
                 </div>
                 <div className="row bordero">
                     <div className="container mainbox">
-                        
+                        <BootstrapTable data={ products }>
+                            <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
+                            <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+                            <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+                        </BootstrapTable>
                     </div>
                 </div>
             </div>  
