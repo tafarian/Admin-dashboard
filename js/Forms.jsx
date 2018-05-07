ac77d0583b4afb2e683542c3609a92b9bff9b989
Jakub Wyczesany <ghaelios@gmail.com>
2018-05-07 15:05:47 +0200

    forms update

--- js/Forms.jsx
+++ js/Forms.jsx
@@ -14,14 +14,6 @@ class Forms extends React.Component {
         };
     }
 
-    getValidationState() {
-        const length = this.state.value.length;
-        if (length > 10) return 'success';
-        else if (length > 5) return 'warning';
-        else if (length > 0) return 'error';
-        return null;
-    }
-
     handleChange(e) {
         this.setState({ value: e.target.value });
     }
@@ -45,6 +37,10 @@ class Forms extends React.Component {
                                 <ControlLabel>Email</ControlLabel>{' '}
                                 <FormControl type="email" placeholder="jane.doe@example.com" />
                             </FormGroup>{' '}
+                            <FormGroup controlId="formControlsTextarea">
+                                <ControlLabel>Textarea</ControlLabel>
+                                <FormControl componentClass="textarea" placeholder="textarea" />
+                            </FormGroup>
                         <Button type="submit">Send invitation</Button>
                         </Form>
                     </div>
